d8fda487440731ecf9c889b4bfd5332e2788a257
Jose Neto <joseneto180201@gmail.com>
2022-03-09 18:48:02 +0000

    .

--- readme.md
+++
@@ -1,3 +0,0 @@
-# Be-programmer.
-
-Projeto para ajudar devs iniciantes e não iniciantes a ter acesso á mente de autores de livros reconhecidos no mundo da programação.
--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {resolve} = require("path");
-const app = express();
-
-const frontend = resolve(__dirname, "./frontend/build");
-app.use("/", express.static(frontend));
-
-app.listen(process.env.PORT || 3000);
