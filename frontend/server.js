f543224bd9c3c904761cf686f693356271ffa4bc
Jose Neto <joseneto180201@gmail.com>
2022-03-10 17:33:43 +0000

    .

--- frontend/server.js
+++ frontend/server.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const {resolve} = require("path");
+const {resolve, join} = require("path");
 const app = express();
 
 const frontend = resolve(__dirname, "./build");
-app.get("*", (req, res) => {
-   
-    let url = path.join(__dirname, './build', 'index.html');
-   
-    // if (!url.startsWith('/app/')) // since we're on local windows
-    //   url = url.substring(1);
-    res.sendFile(url);
+
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, './build/index.html'));
   });
-
-
+  
 app.listen(process.env.PORT || 3000);
